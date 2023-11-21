6cc45bbddd0587ce4d243f52e7deaf7e58d10443
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-11-21 17:01:31 +0530

    Training Pipeline

---
+++ src/Healthcare/pipelines/trainig_pipeline.py
@@ -0,0 +1,31 @@
+import os 
+import sys
+from src.Healthcare.logger import logging
+from src.Healthcare.exception import CustomException
+
+
+from src.Healthcare.components.data_ingestion import DataIngestion,DataIngestionConfig
+from src.Healthcare.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.Healthcare.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
+
+
+if __name__=='__main__':
+    logging.info("The execution has started")
+
+    try:
+        #These line of code will excute data ingestion file:
+        data_ingestion = DataIngestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_config()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        #model trainer
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+    except Exception as e:
+        raise CustomException(e,sys)
