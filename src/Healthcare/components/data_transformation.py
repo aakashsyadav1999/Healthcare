262cf794496944af6b2e0f15d59c640ce4992bf3
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-11-22 22:57:12 +0530

    Data Transformation

--- src/Healthcare/components/data_transformation.py
+++ src/Healthcare/components/data_transformation.py
@@ -34,7 +34,20 @@ class DataTransformation:
 
         self.data_transformation_config = DataTransformationConfig()
 
-        
+    def feature_selection(self,data):
+        """
+        This function is responsible to get those feature which is important for the model
+        """
+        try:
+            logging.info("Entered in feature selection method")
+            data = data.drop(['Date_of_Admission','Billing_Amount','Discharge_Date','Name'], axis =1)
+            data = data.reset_index(drop=True)
+            return data
+        except Exception as e:
+            raise CustomException(e,sys)    
+    
+    
+    
     
     #Features Encoding
     def features_encoding(self, x, y):
@@ -46,7 +59,7 @@ class DataTransformation:
             logging.info("Entered in data encoding method with Target Encoding")
             encoder = TargetEncoder()
             encoder = encoder.fit(x, y)
-            encoded_data = encoder.transform(x)
+            #encoded_data = encoder.transform(x)
 
             logging.info("Encoding completed!!")
             return encoder
@@ -81,7 +94,7 @@ class DataTransformation:
                 'Medical_Condition',
                 'Doctor',
                 'Hospital',
-                'Insurance_Provide',
+                'Insurance_Provider',
                 'Admission_Type',
                 'Medication',
             ]
@@ -116,8 +129,8 @@ class DataTransformation:
                 ]
 
             )
-
-            preprocessor.fit(x,y)
+            
+            preprocessor.fit(x,y=y)
             
             return preprocessor
 
@@ -125,44 +138,67 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
         
-    def initiate_data_transformation(self,train_path,test_path):
+    def initiate_data_transformation(self, train_path, test_path):
+        """
+        This function is responisble for data transformation
+        """
 
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
+            logging.info("Entered into data transformation method")
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
 
-            logging.info("Reading the train and test file")
 
+            logging.info("Read test and train data completed")
+            
             target_column_name='Test_Results'
+
+            # outliers removals in train and test data
+            # logging.info("Removal of outliers in train and test dataset")
+            # train_df = self.outlier_removal(train_df)
+            # test_df =  self.outlier_removal(test_df)
             
 
-            ## divide the train dataset to independent and dependent feature
+            # feature selection in train and test data
+            logging.info("Feature selection in train and test dataset")
+            train_df = self.feature_selection(train_df)
+            test_df = self.feature_selection(test_df)
+
+            # divide the train dataset to independent and dependent feature
 
             input_features_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
+                   
+            print(input_features_train_df)
+            print(target_feature_train_df)
+            # divide the test dataset to independent and dependent feature
 
-            ## divide the test dataset to independent and dependent feature
-
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            input_features_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
 
             logging.info("Applying Target encoding on train and test dataset")
             encoder_train = self.features_encoding(input_features_train_df,target_feature_train_df)
-            encoder_test= self.features_encoding(input_feature_test_df,target_feature_test_df)
+            encoder_test= self.features_encoding(input_features_test_df,target_feature_test_df)
             input_features_encoded_train_df = encoder_train.transform(input_features_train_df)
-            input_feature_encoded_test_df = encoder_test.transform(input_feature_test_df)
-
+            input_feature_encoded_test_df = encoder_test.transform(input_features_test_df)
             
+            #print(input_features_encoded_train_df)
+            #print(input_feature_encoded_test_df)
+
+            # logging.info("Applying oversampling technique-SMOTE only in training dataset")
+            # input_features_sampled_train_df, target_feature_sampled_train_df = self.oversmapling_smote(input_features_encoded_train_df,target_feature_train_df)
+
             logging.info("Scaling of the training and test dataset")
             scaler = self.standardization()
             input_features_scaled_train_arr = scaler.fit_transform(input_features_encoded_train_df)
             input_feature_scaled_test_arr = scaler.transform(input_feature_encoded_test_df)
 
+            print(input_features_scaled_train_arr)
+            print(input_feature_scaled_test_arr)
+
+
             #creating preprocessing obj so while prediction we can preprocess
             preprocessing_obj = self.get_data_transformation_object(input_features_train_df,target_feature_train_df)
-
-            # input_features_scaled_train_arr=preprocessing_obj.fit_transform(input_features_train_df)
-            # input_feature_scaled_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
 
             train_arr = np.c_[
